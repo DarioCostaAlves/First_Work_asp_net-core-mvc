d21529ca6aecf042d415849fa58c5c5f72c037f6
DarioCostaAlves <dario.c.alves22@gmail.com>
2020-07-09 15:35:58 +0100

    Update Seller and Costum Service Exception

---
+++ SalesWebMvc/Services/Exceptions/DBConcurrencyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DBConcurrencyException : ApplicationException
+    {
+        public DBConcurrencyException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        //Ao termos excepções personalizadas teremos um controlo maior de como tratar as que podem ocorrer
+        public NotFoundException(string message) : base(message)
+        {
+            
+        }
+    }
+}
